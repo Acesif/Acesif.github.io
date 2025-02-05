d26dbe61c5a254763bddb5d22805b18dd067cb1d
Acesif <asifpalak@gmail.com>
2025-02-05 13:58:36 +0000

    Deploying to gh-pages from @ Acesif/Acesif.github.io@9a036dcd2122e20de455ed1b9012a14a18ffb6b7 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,13 +16,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "nav-projects",
-          title: "projects",
-          description: "A growing collection of my fun projects.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/projects/";
-          },
         },{id: "nav-repositories",
           title: "repositories",
           description: "",
